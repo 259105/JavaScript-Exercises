9e4baf674f57c0ce29aa1c45818b342b0d6c341d
MICHELE CERRA s301117 <s301117@studenti.polito.it>
2022-03-04 20:40:31 +0100

    course_list.js v0

--- random/course_list.js
+++ random/course_list.js
@@ -1,1 +1,19 @@
-"strict"
+"use strict";
+
+let coursesS = "Web Applications I, Computer Architectures, Data Science and Database Technology, Computer network technologies and services, Information systems security, Software engineering, System and device programming";
+
+const courses = coursesS.split(",");
+for(let i=0 ;i<courses.length;i++){
+    courses[i] = courses[i].trim();
+}
+
+const acronysm = [...courses];
+for(let i=0; i<acronysm.length; i++){
+    const wordsCourse = acronysm[i].split(" ");
+    acronysm[i]="";
+    for(let word of wordsCourse){
+        acronysm[i]+=word[0].toUpperCase();
+    }
+}
+
+console.log(acronysm.sort())
