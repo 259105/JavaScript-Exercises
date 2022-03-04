e0e82a93fcc939b6468a26dcd8f2ff20f32b422b
MICHELE CERRA s301117 <s301117@studenti.polito.it>
2022-03-04 20:47:02 +0100

    Modified course_list.js: (1) const to the string (2) changed the way in which the acronysm list is created, from a normal for to a foreach

--- random/course_list.js
+++ random/course_list.js
@@ -1,19 +1,20 @@
 "use strict";
 
-let coursesS = "Web Applications I, Computer Architectures, Data Science and Database Technology, Computer network technologies and services, Information systems security, Software engineering, System and device programming";
+const coursesS = "Web Applications I, Computer Architectures, Data Science and Database Technology, Computer network technologies and services, Information systems security, Software engineering, System and device programming";
 
 const courses = coursesS.split(",");
 for(let i=0 ;i<courses.length;i++){
     courses[i] = courses[i].trim();
 }
 
-const acronysm = [...courses];
-for(let i=0; i<acronysm.length; i++){
-    const wordsCourse = acronysm[i].split(" ");
-    acronysm[i]="";
+const acronysm = [];
+for(let course of courses){
+    const wordsCourse = course.split(" ");
+    let acro="";
     for(let word of wordsCourse){
-        acronysm[i]+=word[0].toUpperCase();
+        acro+=word[0];
     }
+    acronysm.push(acro.toUpperCase());
 }
 
 console.log(acronysm.sort())
